ddc9c0c2800a9cdffa010b5e8fb280acf0d96583
xiaobin <xiaobin.zhang@dpvr.cn>
2017-12-15 14:42:00 +0800

    组件ID

--- module/local/downloader/downloader.go
+++ module/local/downloader/downloader.go
@@ -1,7 +1,57 @@
 package downloader
 
 import (
-	"go_spider/module"
-	"gopcp.v2/helper/log"
 	"net/http"
+
+	"go_spider/module"
+	"go_spider/module/stub"
+	"gopcp.v2/helper/log"
 )
+
+var logger = log.DLogger()
+
+// /下载器的实现模型
+type myDownloader struct {
+	//组件基础实例  包含 module/base.go
+	stub.ModuleInternal
+	//http客户端
+	httpClient http.Client
+}
+
+func New(
+	mid module.MID,
+	client *http.Client,
+	scoreCalculator module.CalculateScore) (module.Downloader, error) {
+	moduleBase, err := stub.NewModuleInternal(mid, scoreCalculator)
+	if err != nil {
+		return nil, err
+	}
+
+	if client == nil {
+		return nil, genParameterError("nil http client")
+	}
+	return &myDownloader{
+		ModuleInternal: moduleBase,
+		httpClient:     *client,
+	}, nil
+}
+
+func (downloader *myDownloader) Download(req *module.Request) (*module.Response, error) {
+	downloader.ModuleInternal.IncrHandlingNumber()
+	defer downloader.ModuleInternal.DecrHandlingNumber()
+	downloader.ModuleInternal.IncrCalledCount()
+	if req == nil {
+		return nil, genParameterError("nil request")
+	}
+	//data.go 获取http请求
+	httpReq := req.httpReq()
+	downloader.ModuleInternal.IncrAcceptedCount()
+	logger.Infof("Do the request (URL: %s, depth: %d)... \n", httpReq.URL, req.Depth())
+	httpResp, err := downloader.httpClient.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+
+	downloader.ModuleInternal.IncrCompletedCount()
+	return module.NewResponse(httpResp, req.Depth()), nil
+}
--- module/mid.go
+++ module/mid.go
@@ -9,16 +9,101 @@ import (
 	"go_spider/errors"
 )
 
+//代表默认的组件序列号生成器
 var DefaultSNGen = NewSNGenertor(1, 0)
+
+//代表组件ID的模板
+var midTemplate = "%s%d|%s"
 
 type MID string
 
-func GenMID() error {
-	mtype := "string"
-	errMsg := fmt.Sprintf("illegal module type: %s", mtype)
-	return errors.NewIllegalParameterError(errMsg)
+//根据给定参数生成组件ID
+func GenMID(mtype Type, sn uint64, maddr net.Addr) (MID, error) {
+	if !LegalType(mtype) {
+		errMsg := fmt.Sprintf("illegal module type: %s", mtype)
+		return "", errors.NewIllegalParameterError(errMsg)
+	}
+
+	letter := legalTypeLetterMap[mtype]
+	var midStr string
+	if maddr == nil {
+		midStr = fmt.Sprintf(midTemplate, letter, sn, "")
+		midStr = midStr[:len(midStr)-1]
+	} else {
+		midStr = fmt.Sprintf(midTemplate, letter, sn, maddr.String())
+	}
+	return MID(midStr), nil
 }
 
-func SplitMID(mid MID) ([]string, error) {
+//判断给定的组件ID是否合法
+func LegalMID(mid MID) bool {
+	if _, err := SplitMID(mid); err == nil {
+		return true
+	}
+	return false
+}
 
+//用于分解组件ID
+func SplitMID(mid MID) ([]string, error) {
+	// 第一个结果值表示分解是否成功。
+	// 若分解成功，则第二个结果值长度为3，
+	// 并依次包含组件类型字母、序列号和组件网络地址（如果有的话）。
+	var ok bool
+	var letter string
+	var snStr string
+	var addr string
+	midStr := string(mid)
+	if len(midStr) <= 1 {
+		return nil, errors.NewIllegalParameterError("insufficient MID")
+	}
+	// /字符串不能修改 但是可以进行切片操作
+	letter = midStr[:1]
+	if _, ok = legalLetterTypeMap[letter]; !ok {
+		return nil, errors.NewIllegalParameterError(fmt.Sprintf("illegal module type letter: %s", letter))
+	}
+
+	snAndAddr := midStr[1:]
+	index := strings.LastIndex(snAndAddr, "|")
+
+	if index < 0 {
+		snStr = snAndAddr
+		if !legalSN(snStr) {
+			return nil, errors.NewIllegalParameterError(
+				fmt.Sprintf("illegal module SN: %s", snStr))
+		}
+	} else {
+		snStr = snAndAddr[:index]
+		if !legalSN(snStr) {
+			return nil, errors.NewIllegalParameterError(
+				fmt.Sprintf("illegal module SN: %s", snStr))
+		}
+		addr = snAndAddr[index+1:]
+		index = strings.LastIndex(addr, ":")
+		if index <= 0 {
+			return nil, errors.NewIllegalParameterError(
+				fmt.Sprintf("illegal module address: %s", addr))
+		}
+		ipStr := addr[:index]
+		if ip := net.ParseIP(ipStr); ip == nil {
+			return nil, errors.NewIllegalParameterError(
+				fmt.Sprintf("illegal module IP: %s", ipStr))
+		}
+		portStr := addr[index+1:]
+		if _, err := strconv.ParseUint(portStr, 10, 64); err != nil {
+			return nil, errors.NewIllegalParameterError(
+				fmt.Sprintf("illegal module port: %s", portStr))
+		}
+	}
+	return []string{letter, snStr, addr}, nil
+}
+
+func legalSN(snStr string) bool {
+	// s：要转换的字符串
+	// base：进位制（2 进制到 36 进制）
+	// bitSize：指定整数类型（0:int、8:int8、16:int16、32:int32、64:int64）
+	_, err := strconv.ParseUint(snStr, 10, 64)
+	if err != nil {
+		return false
+	}
+	return true
 }
